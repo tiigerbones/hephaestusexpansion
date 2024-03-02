591603d7dbf41c1f8b2074e3d69667ca3176a1db
zackartz <zackmyers@lavabit.com>
2024-03-02 12:41:10 -0500

    init commit

---
+++ src/client/java/io/zackmyers/mcmod/AlfiemodClient.java
@@ -0,0 +1,10 @@
+package io.zackmyers.mcmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlfiemodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
