ff086c39a12a62cc947c27bf547e9ed1d852fe62
tiigerbones <tiiger.b@outlook.com>
2025-08-11 14:01:15 -0600

    Adjusted Registry to skip hepheastusplus if mod is disabled without needing to hide them in a comment/ Cleaned up gradle since i kinda of know what im doing now

--- src/main/java/com/tiigerpaws/hephaestusexpansion/HephExRegistry.java
+++ src/main/java/com/tiigerpaws/hephaestusexpansion/HephExRegistry.java
@@ -4,16 +4,18 @@ import io.github.fabricators_of_create.porting_lib.data.ExistingFileHelper;
 import com.tiigerpaws.hephaestusexpansion.datagen.*;
 import com.tiigerpaws.hephaestusexpansion.registry.HephExItemRegistry;
 import com.tiigerpaws.hephaestusexpansion.spritegen.HephaestusExPartSpriteProvider;
-import konsola5.hephaestusplus.spritegen.HephPlusMaterialRenderInfoProvider;
-import konsola5.hephaestusplus.spritegen.HephaestusPlusMaterialSpriteProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.data.DataProvider;
 import slimeknights.tconstruct.library.client.data.TinkerSpriteSourceGenerator;
 import slimeknights.tconstruct.library.client.data.material.GeneratorPartTextureJsonGenerator;
 import slimeknights.tconstruct.library.client.data.material.MaterialPartTextureGenerator;
 import slimeknights.tconstruct.tools.data.material.MaterialRenderInfoProvider;
 import slimeknights.tconstruct.tools.data.sprite.TinkerMaterialSpriteProvider;
 import slimeknights.tconstruct.tools.data.sprite.TinkerPartSpriteProvider;
+
+import java.lang.reflect.Constructor;
 
 import static com.tiigerpaws.hephaestusexpansion.HephaestusExpansion.LOGGER;
 import static com.tiigerpaws.hephaestusexpansion.HephaestusExpansion.MOD_ID;
@@ -25,11 +27,13 @@ public class HephExRegistry {
     }
 
     public static void gatherData(FabricDataGenerator.Pack pack, ExistingFileHelper existingFileHelper) {
-        LOGGER.info("gathering data....");
+        LOGGER.info("Gathering data...");
 
-        FabricTagProvider.BlockTagProvider blockTags = pack.addProvider(HephExBlockTagsProvider::new); // Required, despite not adding any blocks yet.
+        // Always-provided tags
+        FabricTagProvider.BlockTagProvider blockTags = pack.addProvider(HephExBlockTagsProvider::new);
         pack.addProvider((output, registriesFuture) -> new HephExItemTagsProvider(output, registriesFuture, blockTags));
 
+        // Always-provided generators
         pack.addProvider(HephExToolRecipeProvider::new);
         pack.addProvider(HephExMaterialRecipeProvider::new);
         pack.addProvider(HephExStationSlotLayoutProvider::new);
@@ -37,37 +41,47 @@ public class HephExRegistry {
         pack.addProvider(HephExToolDefinitionProvider::new);
         pack.addProvider(HephExModifierProvider::new);
 
-
-        TinkerMaterialSpriteProvider materialSprites = new TinkerMaterialSpriteProvider();
-
-        /* Hephplus
-        HephaestusPlusMaterialSpriteProvider moreToolMats = new HephaestusPlusMaterialSpriteProvider();
-          */
-
-
         TinkerPartSpriteProvider partSprites = new TinkerPartSpriteProvider();
+        TinkerMaterialSpriteProvider materialSprites = new TinkerMaterialSpriteProvider();
         HephaestusExPartSpriteProvider morePartSprites = new HephaestusExPartSpriteProvider();
         pack.addProvider((output, registriesFuture) -> new MaterialRenderInfoProvider(output, materialSprites));
 
-        /* Hephplus
-        pack.addProvider((output, registriesFuture) -> new HephPlusMaterialRenderInfoProvider(output, moreToolMats));
-          */
+        // Optional: HephaestusPlus datagen
+        if (FabricLoader.getInstance().isModLoaded("hephaestusplus")) {
+            LOGGER.info("HephaestusPlus detected, registering additional data providers...");
+            try {
+                // Instantiate the sprite provider via reflection
+                Class<?> spriteProviderClass = Class.forName("konsola5.hephaestusplus.spritegen.HephaestusPlusMaterialSpriteProvider");
+                Object moreToolMats = spriteProviderClass.getDeclaredConstructor().newInstance();
 
+                // Instantiate the render info provider via reflection
+                Class<?> renderInfoProviderClass = Class.forName("konsola5.hephaestusplus.spritegen.HephPlusMaterialRenderInfoProvider");
+                pack.addProvider((output, registriesFuture) -> {
+                    try {
+                        Constructor<?> ctor = renderInfoProviderClass.getDeclaredConstructors()[0];
+                        return (DataProvider) ctor.newInstance(output, moreToolMats); // <-- cast here
+                    } catch (Exception e) {
+                        throw new RuntimeException("Failed to create HephPlusMaterialRenderInfoProvider", e);
+                    }
+                });
 
+                // MaterialPartTextureGenerator can be created normally because TinkerMaterialSpriteProvider exists
+                pack.addProvider((output, registriesFuture) ->
+                        new MaterialPartTextureGenerator(output, existingFileHelper, morePartSprites, (TinkerMaterialSpriteProvider) moreToolMats)
+                );
+
+            } catch (Exception e) {
+                LOGGER.error("Failed to register HephaestusPlus datagen providers", e);
+            }
+        } else {
+            LOGGER.info("HephaestusPlus not detected, skipping additional data providers.");
+        }
+
+        // Always-provided part textures
         pack.addProvider((output, registriesFuture) -> new GeneratorPartTextureJsonGenerator(output, MOD_ID, morePartSprites));
-
-
-        // Tinkers' materials for HephaestusExpansion parts
         pack.addProvider((output, registriesFuture) -> new MaterialPartTextureGenerator(output, existingFileHelper, morePartSprites, materialSprites));
-
-
-        /* HephaestusPlus materials for HephaestusExpansion parts
-        pack.addProvider((output, registriesFuture) -> new MaterialPartTextureGenerator(output, existingFileHelper, morePartSprites, moreToolMats));
-          */
-
-
         pack.addProvider((output, registriesFuture) -> new TinkerSpriteSourceGenerator(output, existingFileHelper));
-
     }
-}
 
+
+}
