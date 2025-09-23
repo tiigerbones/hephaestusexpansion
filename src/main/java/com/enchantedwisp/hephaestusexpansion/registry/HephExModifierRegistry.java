5eb761747e7d800122b6a89b141a676ab5a63d5e
tiigerbones <tiiger.b@outlook.com>
2025-09-23 12:15:53 -0600

    formatting

--- src/main/java/com/enchantedwisp/hephaestusexpansion/registry/HephExModifierRegistry.java
+++ src/main/java/com/enchantedwisp/hephaestusexpansion/registry/HephExModifierRegistry.java
@@ -8,12 +8,14 @@ import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
 import static com.enchantedwisp.hephaestusexpansion.HephaestusExpansion.MOD_ID;
 
 public class HephExModifierRegistry {
+
+    public static void register() {
+        MODIFIERS.register();
+    }
+
     public static final ModifierDeferredRegister MODIFIERS = ModifierDeferredRegister.create(MOD_ID);
 
     public static final StaticModifier<Modifier> TRUE_EDGE = MODIFIERS.register("true_edge", TrueEdgeModifier::new);
     public static final StaticModifier<Modifier> PIERCING_THRUST = MODIFIERS.register("piercing_thrust", PiercingThrustModifier::new);
     public static final StaticModifier<Modifier> HIGHLAND_WRATH = MODIFIERS.register("highland_wrath", HighlandWrathModifier::new);
-    public static void register() {
-        MODIFIERS.register();
-    }
 }
